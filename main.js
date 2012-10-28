d93bcd6cd299b0a67f096bd8e19e25dc98ccb203
Systwo <starfruitinc@gmail.com>
2012-10-28 16:53:11 -0400

    Removed vim temp files Changed page title to more generic name Added grid drawing function

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
 function getCursorPosition(event) {
-	x  = event.pageX - document.getElementById("can").offsetLeft; //To cancel out padding, and relative to the element, use window.pageXOffset for relative to page
+	//To cancel out padding, and relative to the element, use window.pageXOffset for relative to page
+	x  = event.pageX - document.getElementById("can").offsetLeft; 
 	y  = event.pageY - document.getElementById("can").offsetTop;
 		
 	alert (x + " " + y);
 	return;
 }
 
+function getKeypress(event) {
+	return;
+}
+
+function fillsquare(x, y, can) {
+	var width = 16;
+	var height = 16;
+	var spacing = 2;
+
+	can.fillStyle = "#ffffff";
+	can.fillRect(width*x+spacing*x, height*y+spacing*y, width, height);
+	return;
+}
 
 
 function main() {
@@ -15,9 +29,14 @@ function main() {
 
 	//Add event listeners
 	document.getElementById("can").addEventListener("click", getCursorPosition, false);
-	document.getElementById("can").addEventListener("keypress", "alert('keypress')", false);
+	document.getElementById("can").addEventListener("keypress", getKeypress, false);
 
 	c = document.getElementById("can").getContext("2d");
 	c.rect(00,00,1000,1000);
 	c.fillRect(0,0,1000,1000);
+	fillsquare (5, 5, c);
+	fillsquare (5, 6, c);
+	fillsquare (6, 6, c);
+	fillsquare (6, 5, c);
+
 }
